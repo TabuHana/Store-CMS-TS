40ae800cca941409426a3ac372caf31b6628a08b
tabu <taboodog@hotmail.com>
2023-07-25 23:16:56 -0700

    typescript conv

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const port = 5000;
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    return res.sendStatus(200);
+});
+
+app.listen(port, () => console.log(`App is listening on http://localhost:${port}`));
