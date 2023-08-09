0037247d9a7859ecd6479bbfe843bb3d663c5b9f
tabu <taboodog@hotmail.com>
2023-08-09 09:02:51 -0700

    refactor: moved server create to utils for testing

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,33 +1,19 @@
 // Dep
-import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import config from 'config';
 
-// Files
-import routes from './routes/index.routes';
-
 // Utils
+import createServer from './utils/server';
 import logger from './utils/logger';
 import connect from './utils/connect';
 
-// Middleware
-import deserializedUser from './middleware/deserializedUser';
-
 const port = config.get<number>('port');
 
-const app = express();
-app.use(express.json());
-app.use(deserializedUser)
-
-if (process.env.NODE_ENV === 'production') {
-    console.log('node in production');
-}
+const app = createServer();
 
 app.listen(port, async () => {
     logger.info(`App is running at http://localhost:${port}`);
-    
-    await connect();
 
-    routes(app);
+    await connect();
 });
---
+++ server/src/utils/server.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import deserializedUser from '../middleware/deserializedUser';
+import routes from '../routes/index.routes';
+
+function createServer() {
+  const app = express();
+
+	app.use(express.json());
+	app.use(deserializedUser);
+
+  routes(app);
+
+  return app
+}
+
+export default createServer
