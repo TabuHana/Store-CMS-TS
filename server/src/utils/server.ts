e14d72f62666f227a9598e22e79b1bc016d0ee79
tabu <taboodog@hotmail.com>
2023-08-19 03:16:46 -0700

    check

--- server/src/utils/server.ts
+++ server/src/utils/server.ts
@@ -8,9 +8,12 @@ import cors from 'cors';
 
 function createServer() {
     const app = express();
+    const check = config.get('origin');
+
+    console.log(check);
 
     app.use(cors({ origin: config.get('origin'), credentials: true }));
-    app.use(cookieParser())
+    app.use(cookieParser());
     app.use(express.json());
     app.use(deserializedUser);
 
