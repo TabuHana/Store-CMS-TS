ec05520909bf56b2d6a6322af1b26709c3c7c0f8
tabu <taboodog@hotmail.com>
2023-08-20 20:24:40 -0700

    r10

--- server/src/controller/session.controller.ts
+++ server/src/controller/session.controller.ts
@@ -4,9 +4,8 @@ import { createSession, findSessions, updateSession } from '../service/session.s
 import { signJwt } from '../utils/jwt.utils';
 import config from 'config';
 
-
 export async function createUserSessionHandler(req: Request, res: Response) {
-    console.log('========= CREATE SESSION ==========')
+    console.log('========= CREATE SESSION ==========');
     const user = await validatePassword(req.body);
 
     // Validate the user's password
@@ -14,18 +13,21 @@ export async function createUserSessionHandler(req: Request, res: Response) {
         return res.status(401).send('Invalid email or password');
     }
 
-    console.log(`USER = ${user}`)
+    console.log(`USER = ${user}`);
 
     // Create a session
     const session = await createSession(user.user_id, req.get('user-agent') || '');
 
-    console.log(`SESSION = ${session}`)
+    console.log(`SESSION = ${session}`);
 
-    console.log(`token ttl = ${config.get<string>('accessTokenTtl')}`)
-    console.log(`token type = ${typeof config.get<string>('accessTokenTtl')}`)
+    console.log(`token ttl = ${config.get<string>('accessTokenTtl')}`);
+    console.log(`token type = ${typeof config.get<string>('accessTokenTtl')}`);
 
     // Create an access Token
-    const accessToken = signJwt({ ...user, session: session.session_id }, { expiresIn: config.get<string>('accessTokenTtl') });
+    const accessToken = signJwt(
+        { ...user, session: session.session_id },
+        { expiresIn: config.get<string>('accessTokenTtl') }
+    );
 
     // Create a refresh Token
     const refreshToken = signJwt(
@@ -33,8 +35,8 @@ export async function createUserSessionHandler(req: Request, res: Response) {
         { expiresIn: config.get<string>('refreshTokenTtl') }
     );
 
-    console.log(`expected cookie access = ${accessToken}`)
-    console.log(`expected cookie access = ${refreshToken}`)
+    console.log(`expected cookie access = ${accessToken}`);
+    console.log(`expected cookie access = ${refreshToken}`);
 
     res.cookie('accessToken', accessToken, {
         maxAge: 900000, // 15 mins
@@ -42,7 +44,7 @@ export async function createUserSessionHandler(req: Request, res: Response) {
         domain: config.get('origin'),
         path: '/',
         sameSite: 'strict',
-        secure: false, //set to true for prod
+        secure: true, //set to true for prod
     });
 
     res.cookie('refreshToken', refreshToken, {
@@ -51,9 +53,10 @@ export async function createUserSessionHandler(req: Request, res: Response) {
         domain: config.get('origin'),
         path: '/',
         sameSite: 'strict',
-        secure: false, //set to true for prod
+        secure: true, //set to true for prod
     });
 
+    console.log('Cookies should be set now');
     // Return access & refresh Token
     return res.status(200).send({ accessToken, refreshToken });
 }
