3b1db85f6e24f5084d719c9407ac323e599092f7
tabu <taboodog@hotmail.com>
2023-09-01 21:16:24 -0700

    orking in branch for server edits on token

--- client/src/context/AuthContext.tsx
+++ client/src/context/AuthContext.tsx
@@ -5,19 +5,26 @@ type User = {
     password: string;
 };
 
+type Auth = {
+    user: User;
+    accessToken: string
+}
+
 type RegisterType = {
     name: string;
     email: string;
     password: string;
     passwordConfirmation: string;
-}
+};
 
 export type AuthContextType = {
+    auth: Auth | null;
+    setAuth: React.Dispatch<React.SetStateAction<Auth | null>>;
     user: any;
-    setUser: any;
+    setUser: React.Dispatch<React.SetStateAction<User | null>>;
     login: (user: User) => void;
     logout: () => void;
-    register: (data: RegisterType) => void
+    register: (data: RegisterType) => void;
 };
 
 type AuthProviderType = {
@@ -28,9 +35,10 @@ export const AuthContext = createContext({} as AuthContextType);
 
 export const AuthProvider = ({ children }: AuthProviderType) => {
     const [user, setUser] = useState<User | null>(null);
+    const [auth, setAuth] = useState<Auth | null>(null)
 
     const login = (user: User) => {
-        console.log(user)
+        console.log(user);
     };
 
     const logout = () => {
@@ -38,8 +46,10 @@ export const AuthProvider = ({ children }: AuthProviderType) => {
     };
 
     const register = (data: RegisterType) => {
-        console.log(data)
-    }
+        console.log(data);
+    };
 
-    return <AuthContext.Provider value={{ user, setUser, login, logout, register }}>{children}</AuthContext.Provider>;
+    return <AuthContext.Provider value={{ auth, setAuth, user, setUser, login, logout, register }}>{children}</AuthContext.Provider>;
 };
+
+// working in branch for server edits on token
